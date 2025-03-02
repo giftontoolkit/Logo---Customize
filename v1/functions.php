c1e80cc8320bcc08610f1ff9bddade6f59bcdc80
giftontoolkit <giftontoolkit@gmail.com>
2025-03-02 06:33:05 +0500

    Add files via upload

--- v1/functions.php
+++ v1/functions.php
@@ -1,8 +1,8 @@
 <?php
 // Theme Setup - Logo
-function gifton_theme_setup_logo() {
+function myTM_theme_setup_logo() {
     // Enable support for custom logo
     add_theme_support('custom-logo');
 }
-add_action('after_setup_theme', 'gifton_theme_setup_logo');
+add_action('after_setup_theme', 'myTM_theme_setup_logo');
 ?>
